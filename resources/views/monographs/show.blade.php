24595149d8fe229553bf037a5534a29730e06315
alonsofdez23 <alonsobs23@gmail.com>
2022-04-27 19:25:04 +0200

    corregido dark mode en show de Monograph

--- resources/views/monographs/show.blade.php
+++ resources/views/monographs/show.blade.php
@@ -12,17 +12,17 @@
                     <div class="flex flex-col items-center mt-4">
                         <div class="flex flex-col items-center md:flex-row md:max-w-xl">
                             <div class="flex flex-col justify-between p-4 leading-normal">
-                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $monograph_articles->title }}</h5>
-                                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ $monograph_articles->year }}</p>
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{{ $monograph_articles->title }}</h5>
+                                <p class="mb-3 font-normal text-gray-700">{{ $monograph_articles->year }}</p>
 
                                 @foreach ($monograph_articles->articles as $article)
-                                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ $article->title }}</p>
+                                <p class="mb-3 font-normal text-gray-700">{{ $article->title }}</p>
                                 @endforeach
 
                                 @if ($monograph_articles->articles->count() == 0)
-                                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Esta monografía no contiene artículos</p>
+                                    <p class="mb-3 font-normal text-gray-700">Esta monografía no contiene artículos</p>
                                 @else
-                                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Numero total de páginas de todos los artículos: <span class="font-bold">{{ $monograph_articles->articles_sum_number_pages }}</span></p>
+                                    <p class="mb-3 font-normal text-gray-700">Numero total de páginas de todos los artículos: <span class="font-bold">{{ $monograph_articles->articles_sum_number_pages }}</span></p>
                                 @endif
                             </div>
                         </div>
